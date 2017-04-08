74f4a08a58f1bfcc0ac23b89761a4d7087767922
Billy Barbaro <billy@thebarbaros.com>
2017-04-07 21:49:01 -0400

    Left a todo.

--- server.py
+++ server.py
@@ -4,16 +4,15 @@ app = Flask(__name__)
 
 QUERY_TEMPLATE = 'http://data.fcc.gov/api/block/2010/find?format=json&latitude={}&longitude=}{}'
 
+def get_tract(latitude, longitude):
+    #TODO: DAVE!
+
 @app.route('/', methods='POST')
 def hello_world():
     locations = request.json['locations']
     tracts = []
     for location in locations:
-        lat, lng = location['lat'], location['lng']
-        query_string = QUERY_TEMPLATE.format(lat, lng)
-        response = requests.get(query_string).json()
-        full_code = response['Block']['FIPS']
-        tracts.append(full_code[5:11])
+        tracts.append(get_tract(location['lat'], location['lng']))
     output = ''
     for tract in tracts:
         output += tract + '\n'
