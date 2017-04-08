f72f034c3571f17f82770a90ceeb1572fb055053
Billy Barbaro <billy@thebarbaros.com>
2017-04-07 21:46:48 -0400

    Drafted conversion from lat/lng to tract.

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from flask import Flask
+import requests
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
+QUERY_TEMPLATE = 'http://data.fcc.gov/api/block/2010/find?format=json&latitude={}&longitude=}{}'
+
+@app.route('/', methods='POST')
 def hello_world():
-    return 'Hello, World!'
+    locations = request.json['locations']
+    tracts = []
+    for location in locations:
+        lat, lng = location['lat'], location['lng']
+        query_string = QUERY_TEMPLATE.format(lat, lng)
+        response = requests.get(query_string).json()
+        full_code = response['Block']['FIPS']
+        tracts.append(full_code[5:11])
+    output = ''
+    for tract in tracts:
+        output += tract + '\n'
+    return output
